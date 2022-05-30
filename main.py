806eb54e15c513b899b611b1a2a4c5c2a3102ee0
Thomas COLIN <thomas.colin@le-campus-numerique.fr>
2022-05-30 11:37:21 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# main.py
+
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class Device(BaseModel):
+    deviceID: int
+    deviceName: str
+    deviceType: str
+
+
+@app.post("/add")
+async def add_device(device: Device):
+    return device
+
+@app.delete("/delete/{id}")
+async def delete_device(id: int):
+    return id
+
+@app.get("/devices/{id}")
+async def get_devices(id: int):
+    return id
+
+@app.put("/update/{id}")
+async def update_device(device: Device):
+    return device
