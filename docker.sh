10101609912b002b9d0b8ddca979d7a3c5afbcc1
denis160 <beliant.denis@gmail.com>
2022-06-01 09:13:47 +0200

    docker compose work

--- docker.sh
+++ docker.sh
@@ -1,15 +1,28 @@
 # !/bin/bash
 echo "jefasse tout"
-sudo docker container stop test
+sudo docker stop test2
+sudo docker container rm test2
 
 echo "pull"
 sudo docker pull tiangolo/uvicorn-gunicorn-fastapi
 
-echo "runner latopeuh"
-sudo docker run -d --name test2 -p 0.0.0.0:80:80 tiangolo/uvicorn-gunicorn-fastapi:latest
+sudo docker run -d --name test2 -v /:/app tiangolo/uvicorn-gunicorn-fastapi
 
-echo "coupi"
-sudo docker cp main.py test2:app/
+#echo "run run run"
+#sudo docker run -d --name test2 -p 0.0.0.0:80:80 -v /:/app tiangolo/uvicorn-gunicorn-fastapi
 
-echo "restare"
-sudo docker restart test2
+#echo "install dependencies"
+#sudo docker exec test2 /usr/local/bin/pip3 install "fastapi[all]" 
+
+#echo "coupi"
+#sudo docker cp main.py test2:app/
+
+#echo "stoupi"
+#sudo docker stop test2
+
+#echo "runner latopeuh"
+#sudo docker run -d --name test2 -p 0.0.0.0:80:80 -v ./:/app
+#sudo docker start test2
+
+#echo "restare"
+#sudo docker restart test2
